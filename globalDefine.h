79ee08cd6a94a587ed1f5720038dfac70f96d140
Ryosuke_H <fx.shovel.01to77@gmail.com>
2014-01-09 21:20:30 +0900

    First

---
+++ globalDefine.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   globalDefine.h
+ * Author: MMH
+ *
+ * Created on 2014/01/09, 21:10
+ */
+
+#ifndef GLOBALDEFINE_H
+#define	GLOBALDEFINE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+// define
+#define _XTAL_FREQ  64000000 // 64MHz
+
+// Global variables
+unsigned char mtrPhase;
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* GLOBALDEFINE_H */
+
